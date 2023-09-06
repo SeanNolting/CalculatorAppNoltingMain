b819499e85175dcd2925ce810a894eac22466376
SeanNolting <113694273+SeanNolting@users.noreply.github.com>
2023-09-06 12:55:41 -0500

    added a answer box. Began researching how to make my buttons work
    Continue watching video to make addition button function and see where things go from there

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.calculatorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    EditText num1 = findViewById(R.id.num1et);
+    EditText num2 = findViewById(R.id.num2et);
+    TextView result = findViewById(R.id.answer_box);
+    public void add()
+    {
+       result = num1;
+    }
+
 }
