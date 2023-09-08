8c6520c15161f49ddb444fe4517025a08cbaf2a7
SeanNolting <113694273+SeanNolting@users.noreply.github.com>
2023-09-08 12:55:14 -0500

    Changed my answer to appear in the text box instead of a popup
    Check code for what to work on this weekend

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -7,11 +7,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.text.TextUtils;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-   private EditText int1;
+    private EditText int1;
     private EditText int2 ;
     private Button add, sub, div, mult;
     private TextView answer;
@@ -34,7 +35,9 @@ public class MainActivity extends AppCompatActivity {
                 int num1 = Integer.parseInt(int1.getText().toString());
                 int num2 = Integer.parseInt(int2.getText().toString());
                 int sum = num1 + num2;
-                Toast.makeText(getApplicationContext(), "Sum = " + sum, Toast.LENGTH_SHORT).show();
+                String sumString = String.valueOf(sum);
+                answer.setText(sumString);
+                //Toast.makeText(getApplicationContext(), "Sum = " + sum, Toast.LENGTH_SHORT).show();
             }
         });
         sub.setOnClickListener(new View.OnClickListener() {
@@ -42,8 +45,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 int num1 = Integer.parseInt(int1.getText().toString());
                 int num2 = Integer.parseInt(int2.getText().toString());
+
+                /*
+                ADD something that checks if the text boxes (checks one or both) are empty and prompt the user to enter a number
+                Utilize the isEmpty method to check the text box
+                 */
+                //Toast.makeText(getApplicationContext(), "Enter a number in the box" , Toast.LENGTH_LONG).show();
                 int diff = num1 - num2;
-                Toast.makeText(getApplicationContext(), "Difference = " + diff, Toast.LENGTH_SHORT).show();
+                String diffString = String.valueOf(diff);
+                answer.setText(diffString);
+
             }
         });
         mult.setOnClickListener(new View.OnClickListener() {
@@ -52,7 +63,9 @@ public class MainActivity extends AppCompatActivity {
                 int num1 = Integer.parseInt(int1.getText().toString());
                 int num2 = Integer.parseInt(int2.getText().toString());
                 int prod = num1 * num2;
-                Toast.makeText(getApplicationContext(), "Product = " + prod, Toast.LENGTH_SHORT).show();
+                String prodString = String.valueOf(prod);
+                answer.setText(prodString);
+                //Toast.makeText(getApplicationContext(), "Product = " + prod, Toast.LENGTH_SHORT).show();
             }
         });
         div.setOnClickListener(new View.OnClickListener() {
@@ -61,7 +74,9 @@ public class MainActivity extends AppCompatActivity {
                 double num1 = Integer.parseInt(int1.getText().toString());
                 double num2 = Integer.parseInt(int2.getText().toString());
                 double quot = num1 / num2;
-                Toast.makeText(getApplicationContext(), "Quotient = " + quot, Toast.LENGTH_SHORT).show();
+                String quotString = String.valueOf(quot);
+                answer.setText(quotString);
+                //Toast.makeText(getApplicationContext(), "Quotient = " + quot, Toast.LENGTH_SHORT).show();
             }
         });
     }
