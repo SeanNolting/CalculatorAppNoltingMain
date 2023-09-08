2075a2855aaf047e817ec624e1db1de06c06c4b9
SeanNolting <113694273+SeanNolting@users.noreply.github.com>
2023-09-07 23:15:51 -0500

    Made my buttons work correctly
    Watched a video for extra support on what to do. (Need to site source)
    
    In class
    1. Spice up app (more colors, explore md3)
    2. Improve wire frame (Its grey and boring)
    3. Find a way to make the answer appear in the text box (Video help probably?)

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -7,31 +7,65 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+   private EditText int1;
+    private EditText int2 ;
+    private Button add, sub, div, mult;
+    private TextView answer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        int1 = findViewById(R.id.num1et);
+        int2 = findViewById(R.id.num2et);
+        add = findViewById(R.id.addition);
+        sub = findViewById(R.id.subtraction);
+        div = findViewById(R.id.division);
+        mult = findViewById(R.id.multiplication);
+        answer = findViewById(R.id.answer_box);
 
-    EditText int1 = findViewById(R.id.num1et);
-    EditText int2 = findViewById(R.id.num2et);
-    Button add = findViewById(R.id.addition);
-    TextView answer = findViewById(R.id.answer_box);
-
-    add.setOnClickListener(new View.OnClickListener)(
-    {
-        public void findSum(View view)
-        {
-            int num1 = Integer.parseInt(int1.getText().toString());
-            int num2 = Integer.parseInt(int2.getText().toString());
-            int sum = num1 + num2;
-            answer.setText(sum);
-        }
-    })
-
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int num1 = Integer.parseInt(int1.getText().toString());
+                int num2 = Integer.parseInt(int2.getText().toString());
+                int sum = num1 + num2;
+                Toast.makeText(getApplicationContext(), "Sum = " + sum, Toast.LENGTH_SHORT).show();
+            }
+        });
+        sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int num1 = Integer.parseInt(int1.getText().toString());
+                int num2 = Integer.parseInt(int2.getText().toString());
+                int diff = num1 - num2;
+                Toast.makeText(getApplicationContext(), "Difference = " + diff, Toast.LENGTH_SHORT).show();
+            }
+        });
+        mult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int num1 = Integer.parseInt(int1.getText().toString());
+                int num2 = Integer.parseInt(int2.getText().toString());
+                int prod = num1 * num2;
+                Toast.makeText(getApplicationContext(), "Product = " + prod, Toast.LENGTH_SHORT).show();
+            }
+        });
+        div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double num1 = Integer.parseInt(int1.getText().toString());
+                double num2 = Integer.parseInt(int2.getText().toString());
+                double quot = num1 / num2;
+                Toast.makeText(getApplicationContext(), "Quotient = " + quot, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
+
+
