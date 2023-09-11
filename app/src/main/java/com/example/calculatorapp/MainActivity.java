ed0ea2d08f2010ea0032f1b3f23e1e09d202e7dd
SeanNolting <113694273+SeanNolting@users.noreply.github.com>
2023-09-10 22:08:57 -0500

    Added a clear button and added a way to check if the text boxes are empty
    Use class time to improve the look of the app (Dimens, padding, margins, text, colors)
    Focus on getting the simple things right (Functionality and UI UX)
    
    Use at home time for more difficult stuff
    Possibly make a second screen with a nine digit number pad with operations.
    (Think about it would be like answer box text += num1 + operation + num2) and then clear button and stuff

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.calculatorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,7 +16,10 @@ public class MainActivity extends AppCompatActivity {
     private EditText int1;
     private EditText int2 ;
     private Button add, sub, div, mult;
+    private Button clear;
     private TextView answer;
+    String blankBox = "Answer";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,55 +32,102 @@ public class MainActivity extends AppCompatActivity {
         div = findViewById(R.id.division);
         mult = findViewById(R.id.multiplication);
         answer = findViewById(R.id.answer_box);
+        clear = findViewById(R.id.clear);
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int num1 = Integer.parseInt(int1.getText().toString());
-                int num2 = Integer.parseInt(int2.getText().toString());
-                int sum = num1 + num2;
-                String sumString = String.valueOf(sum);
-                answer.setText(sumString);
-                //Toast.makeText(getApplicationContext(), "Sum = " + sum, Toast.LENGTH_SHORT).show();
+                if(TextUtils.isEmpty(int1.getText().toString()))
+                {
+                    Toast.makeText(getApplicationContext(), "Enter a number in the box" , Toast.LENGTH_LONG).show();
+                    answer.setText(blankBox);
+                }
+                else if (TextUtils.isEmpty(int2.getText().toString()))
+                {
+                    Toast.makeText(getApplicationContext(), "Enter a number in the box" , Toast.LENGTH_LONG).show();
+                    answer.setText(blankBox);
+                }
+                else
+                {
+                        int num1 = Integer.parseInt(int1.getText().toString());
+                        int num2 = Integer.parseInt(int2.getText().toString());
+                        int sum = num1 + num2;
+                        String sumString = String.valueOf(sum);
+                        answer.setText(sumString);
+                }
             }
         });
         sub.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                int num1 = Integer.parseInt(int1.getText().toString());
-                int num2 = Integer.parseInt(int2.getText().toString());
-
-                /*
-                ADD something that checks if the text boxes (checks one or both) are empty and prompt the user to enter a number
-                Utilize the isEmpty method to check the text box
-                 */
-                //Toast.makeText(getApplicationContext(), "Enter a number in the box" , Toast.LENGTH_LONG).show();
-                int diff = num1 - num2;
-                String diffString = String.valueOf(diff);
-                answer.setText(diffString);
+            public void onClick(View view)
+            {
+                if(TextUtils.isEmpty(int1.getText().toString()))
+                {
+                    Toast.makeText(getApplicationContext(), "Enter a number in the box" , Toast.LENGTH_LONG).show();
+                }
+                else if (TextUtils.isEmpty(int2.getText().toString()))
+                {
+                    Toast.makeText(getApplicationContext(), "Enter a number in the box" , Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    int num1 = Integer.parseInt(int1.getText().toString());
+                    int num2 = Integer.parseInt(int2.getText().toString());
+                    int diff = num1 - num2;
+                    String diffString = String.valueOf(diff);
+                    answer.setText(diffString);
+                }
 
             }
         });
         mult.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int num1 = Integer.parseInt(int1.getText().toString());
-                int num2 = Integer.parseInt(int2.getText().toString());
-                int prod = num1 * num2;
-                String prodString = String.valueOf(prod);
-                answer.setText(prodString);
-                //Toast.makeText(getApplicationContext(), "Product = " + prod, Toast.LENGTH_SHORT).show();
+                if(TextUtils.isEmpty(int1.getText().toString()))
+                {
+                    Toast.makeText(getApplicationContext(), "Enter a number in the box" , Toast.LENGTH_LONG).show();
+                }
+                else if (TextUtils.isEmpty(int2.getText().toString()))
+                {
+                    Toast.makeText(getApplicationContext(), "Enter a number in the box" , Toast.LENGTH_LONG).show();
+                }
+                else {
+                    int num1 = Integer.parseInt(int1.getText().toString());
+                    int num2 = Integer.parseInt(int2.getText().toString());
+                    int prod = num1 * num2;
+                    String prodString = String.valueOf(prod);
+                    answer.setText(prodString);
+                }
             }
         });
         div.setOnClickListener(new View.OnClickListener() {
             @Override
+            public void onClick(View view)
+            {
+                if(TextUtils.isEmpty(int1.getText().toString()))
+                {
+                    Toast.makeText(getApplicationContext(), "Enter a number in the box" , Toast.LENGTH_LONG).show();
+                }
+                else if (TextUtils.isEmpty(int2.getText().toString()))
+                {
+                    Toast.makeText(getApplicationContext(), "Enter a number in the box" , Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    double num1 = Integer.parseInt(int1.getText().toString());
+                    double num2 = Integer.parseInt(int2.getText().toString());
+                    double quot = num1 / num2;
+                    String quotString = String.valueOf(quot);
+                    answer.setText(quotString);
+                }
+            }
+        });
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View view) {
-                double num1 = Integer.parseInt(int1.getText().toString());
-                double num2 = Integer.parseInt(int2.getText().toString());
-                double quot = num1 / num2;
-                String quotString = String.valueOf(quot);
-                answer.setText(quotString);
-                //Toast.makeText(getApplicationContext(), "Quotient = " + quot, Toast.LENGTH_SHORT).show();
+                int1.setText("");
+                int2.setText("");
+                answer.setText("Answer");
             }
         });
     }
